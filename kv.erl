0a3f526e42e87d5a1c882e103ff5b42b3ed7a804
ara4711 <andreas.rasmusson@gmail.com>
2014-09-05 20:42:19 +0200

    bugfix2

--- kv.erl
+++ kv.erl
@@ -29,17 +29,17 @@ server(T) ->
 
 insert(K,V,leaf) ->
   {node,leaf,K,V,leaf};
-insert(K,V,{node,L,KN,_VN,R}) ->
+insert(K,V,{node,L,KN,VN,R}) ->
   if K<KN ->
-      insert(K,V,L);
+      {node, insert(K,V,L), KN, VN, R }; 
      K==KN ->
       {node,L,K,V,R};
      K>KN ->
-      insert(K,V,R)
+      {node, L, KN, VN, insert(K,V,R) }
   end.
 
 lookup(_,leaf) ->
-  falsefoo;
+  false;
 lookup(K,{node,L,KN,VN,R}) ->
   if K<KN ->
       lookup(K,L);
